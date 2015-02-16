dbd31ad8b0cc33f626851ccdafdfb98e288a47d2
jhermann <jh@web.de>
2015-02-16 16:19:08 +0100

    added a generally available logger fixture

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -16,8 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+
 import pytest
 
 
 # Globally available fixtures
-
+@pytest.fixture
+def logger():
+    """Test logger instance as a fixture."""
+    logging.basicConfig(level=logging.DEBUG)
+    return logging.getLogger('tests')
